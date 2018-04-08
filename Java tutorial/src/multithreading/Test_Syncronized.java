9a2a67aec363364d01289695fc6144ce9104715d
amit_kaushik <ak40717@gmail.com>
2018-04-09 01:13:40 +0530

    comments changes

--- Java tutorial/src/multithreading/Test_Syncronized.java
+++ Java tutorial/src/multithreading/Test_Syncronized.java
@@ -50,39 +50,39 @@ public class Test_Syncronized {
 
  /*case 1 : method get() is void  && method put() is void
   	output :put method called
-			get method called
-			get method called
-			put method called
-			.......
+get method called
+get method called
+put method called
+.......
    case 2 : method get() is synchronized & void  &&  method put() is void
   	output :put method called
-			get method called
-			get method called
-			put method called
-			........
+get method called
+get method called
+put method called
+........
    case 3 : method get() is synchronized & void  &&  method put() is synchronized & void
   	output :get method called
-			get method called
-			get method called
-			get method called
-			.......
+get method called
+get method called
+get method called
+.......
    case 4 : method get() is static & synchronized & void  &&  method put() is synchronized & void
-   			SAME AS : method get() is static & synchronized & void  &&  method put() is void
+	SAME AS : method get() is static & synchronized & void  &&  method put() is void
   	output :put method called
-			get method called
-			get method called
-			put method called
-			.......
-			(static methods lock on the class object of the class. Non-static methods lock on the instance
-			 on which they're called (by default, synchronized(anyOtherLock) is also possible). 
-			 Since they lock on different objects, they don't block each other.)
+get method called
+get method called
+put method called
+.......
+(static methods lock on the class object of the class. Non-static methods lock on the instance
+on which they're called (by default, synchronized(anyOtherLock) is also possible). 
+Since they lock on different objects, they don't block each other.)
 				
    case 5 : method get() is static & synchronized & void  &&  method put() is static & synchronized & void
   	output :put method called
-			put method called
-			put method called
-			put method called
-			........
+put method called
+put method called
+put method called
+........
 			
 Note : 1) (I)synchronized void test() { foo(); } EQUALS (II) void test() { synchronized(this) { foo(); } 
        2) (I)static synchronized void test() { foo(); } EQUALS (II)static void test() { synchronized(MyClass.class) { foo(); } }
